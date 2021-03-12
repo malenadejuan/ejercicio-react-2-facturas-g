dc7b85b634b9e86093d49be058c5dc05f78a3205
Malena de Juan <malena.dj4@gmail.com>
2021-03-12 21:16:23 +0100

    reemplazado Filas en App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Container, Row, Col, Form, Table } from "react-bootstrap";
 import Busqueda from "./Busqueda";
+import Filas from "./Filas";
 
 function App() {
   return (
@@ -24,27 +25,7 @@ function App() {
               <th className="col-max">Vence</th>
             </tr>
           </thead>
-          <tbody>
-            <tr className="factura factura-dummy">
-              <td className="numero"></td>
-              <td className="fecha"></td>
-              <td className="concepto"></td>
-              <td><span className="base"></span>€</td>
-              <td><span className="cantidad-iva"></span>€ (<span className="tipo-iva"></span>%)</td>
-              <td><span className="total"></span>€</td>
-              <td className="estado"></td>
-              <td className="vencimiento"></td>
-            </tr>
-          </tbody>
-          <tfoot>
-            <tr className="totales">
-              <th className="text-right" colspan="3">Totales:</th>
-              <td><span className="total-bases"></span>€</td>
-              <td><span className="total-ivas"></span>€</td>
-              <td><span className="total-totales"></span>€</td>
-              <td colspan="2"></td>
-            </tr>
-          </tfoot>
+          <Filas></Filas>
         </Table>
       </main>
     </Container>
