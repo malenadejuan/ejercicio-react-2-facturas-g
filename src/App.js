adb4a020aad05a3dd3a39f55c7c15cecf2612e8e
Malena de Juan <malena.dj4@gmail.com>
2021-03-12 21:19:46 +0100

    llenado componente Global

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
-import { Container, Row, Col, Form, Table } from "react-bootstrap";
-import Busqueda from "./Busqueda";
-import Filas from "./Filas";
+import Global from "./Global";
 
 function App() {
   return (
-    <Container as="section" fluid className="principal">
-      <Row as="header" className="cabecera">
-        <Col>
-          <h2>Listado de ingresos</h2>
-        </Col>
-      </Row>
-      <main>
-        <Busqueda></Busqueda>
-        <Table bordered hover striped className="listado">
-          <thead className="thead-light">
-            <tr>
-              <th className="col-min">Num.</th>
-              <th className="col-med">Fecha</th>
-              <th className="col-concepto">Concepto</th>
-              <th className="col-med">Base</th>
-              <th className="col-max">IVA</th>
-              <th className="col-med">Total</th>
-              <th className="col-max">Estado</th>
-              <th className="col-max">Vence</th>
-            </tr>
-          </thead>
-          <Filas></Filas>
-        </Table>
-      </main>
-    </Container>
+    <Global></Global>
   );
 }
 
