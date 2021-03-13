c91faa7c971da6cd4fb3d83a1b0ebcf67fbadd9c
Carlos Solano <carlos.solanogomez@hotmail.com>
2021-03-13 15:21:41 +0100

    filas numeros, cuentas

--- src/Filas.js
+++ src/Filas.js
@@ -1,24 +1,32 @@
+import UrlAPI from "./UrlAPI";
+import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { DateTime } from "luxon";
-import UlAPI from "./UrlAPI";
 
-const Filas = () => {
+const Filas = (props) => {
+  const { DatosNumeros, DateTime, NumeroIVA, Vencimiento, DatoVencimiento } = props;
   return (
-    <>
-      <tbody>
-        <tr className="factura factura-dummy">
-          <td className="numero"></td>
-          <td className="fecha"></td>
-          <td className="concepto"></td>
-          <td><span className="base"></span>€</td>
-          <td><span className="cantidad-iva"></span>€ (<span className="tipo-iva"></span>%)</td>
-          <td><span className="total"></span>€</td>
-          <td className="estado"></td>
-          <td className="vencimiento"></td>
-        </tr>
-      </tbody>
-    </>
+    <tbody> {DatosNumeros ? DatosNumeros.map((factura) => (
+      <tr key={DatosNumeros.id} className="factura">
+        <td className="numero">{factura.numero}</td>
+        <td className="fecha">{DateTime.fromMillis(+factura.fecha).toLocaleString()}</td>
+        <td className="concepto">{factura.concepto}</td>
+        <td><span className="base">{factura.base.toFixed(2)}</span>€</td>
+        <td><span className="cantidad-iva">{NumeroIVA(factura.base, factura.tipoIva)}</span>€
+          (<span className="tipo-iva">{factura.tipoIva}</span>%)</td>
+        <td><span className="total">{(factura.base + NumeroIVA(factura.base, factura.tipoIva)).toFixed(2)}</span>€</td>
+        <td className="estado"></td>
+        <td className="vencimiento"></td>
+      </tr>
+    )) : <tr className="factura-dummy" />
+    }
+    </tbody>
   );
 };
 
+Filas.propTypes = {
+  DateTime: PropTypes.instanceOf(DateTime).isRequired,
+  DatosNumeros: PropTypes.array.isRequired,
+  NumeroIVA: PropTypes.func.isRequired,
+};
 export default Filas;
