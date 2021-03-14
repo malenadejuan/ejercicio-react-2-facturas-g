89f680256f5f26f4a86718c9d7f190bf38a6c290
Malena de Juan <malena.dj4@gmail.com>
2021-03-14 13:32:17 +0100

    añadido el prototypes en busqueda

--- src/Busqueda.js
+++ src/Busqueda.js
@@ -1,23 +1,37 @@
+import PropTypes from "prop-types";
 import { useState } from "react";
 import { Row, Col } from "react-bootstrap";
 
-const Busqueda = () => {
+const Busqueda = ({ facturas }) => {
   const [busqueda, setBusqueda] = useState("");
-  const cambiarBusqueda = () => {
-    setBusqueda(busqueda);
+  const [facturasFiltradas, setFacturasFiltradas] = useState([]);
+
+  const cambiarBusqueda = e => {
+    setBusqueda(e.target.value);
+  };
+
+  const buscar = e => {
+    e.preventDefault();
+    console.log(facturas);
   };
 
   return (
     <Row>
       <Col className="info-listado info-listado-top text-right">
-        <form>
+        <form onSubmit={buscar}>
           <label htmlFor="busqueda" >Buscar</label>
           <input id="busqueda" type="text" value={busqueda} onChange={cambiarBusqueda} />
         </form>
       </Col>
-    </Row>
+    </Row >
   );
 };
 
+Busqueda.propTypes = {
+  facturas: PropTypes.array.isRequired
+};
+
 export default Busqueda;
+
+
 
